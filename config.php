62d992daec9d66ed8b786aee37b56629be571861
Zulkarnaen Hatala <dzulqarnaenhatala@gmail.com>
2023-08-30 10:22:07 +0000

    pre

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'md3';
+$CFG->dbuser    = 'joesmart';
+$CFG->dbpass    = '1234';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_0900_ai_ci',
+);
+
+$CFG->wwwroot   = 'http://192.168.8.97/moodle/v339_2019';
+$CFG->dataroot  = '/var/www/moodledata/v339_2019';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
