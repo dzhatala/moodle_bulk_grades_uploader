9a81783eada62e3f51cf84c7b4a576991445c84b
Zulkarnaen Hatala <dzulqarnaenhatala@gmail.com>
2023-08-31 06:26:33 +0000

    pre

--- course/gu_01.php
+++ course/gu_01.php
@@ -8,6 +8,8 @@
 <?php
 
 require_once '../config.php';
+require_once($CFG->libdir . '/gradelib.php'); //
+
  
 require '/var/www/composer/vendor/autoload.php';//absolute values of composer installation...
 require './../grade/edit/tree/total_creator.php';
@@ -15,6 +17,7 @@ require './../grade/edit/tree/total_creator.php';
 // die;
 
 //https://stackoverflow.com/questions/3302857/algorithm-to-get-the-excel-like-column-name-of-a-number
+//get excel name column
 function getNameFromNumber($num) {
     $numeric = $num % 26;
     $letter = chr(65 + $numeric);
@@ -24,6 +27,25 @@ function getNameFromNumber($num) {
     } else {
         return $letter;
     }
+}
+
+/* */
+function getLetterFromPoint($grade_letters,$point){
+	foreach ($grade_letters as $num => $let) {
+		//echo $num ."=>". $let." ";
+		$numreal=floatval($num);
+		if($point>=$numreal)return $let;
+	}
+	
+	return $let; //lowest letter
+		
+}
+
+function getPointFromLetter($grade_letters,$cl){
+	foreach ($grade_letters as $num => $let) {
+		if(trim($let)==trim($cl))return (floatval($num)+1.0);
+	}
+	return 0;
 }
 
 
@@ -47,7 +69,7 @@ $sheetCount = $spreadsheet->getSheetCount();
 $sheetNames = $spreadsheet->getSheetNames();
 foreach ($sheetNames as $sheetIndex => $sheetName) {
     // $helper->log('WorkSheet #' . $sheetIndex . ' is named "' . $sheetName . '"');
-	echo "worksheet ".$sheetIndex ." =>" .$sheetName."<br>";
+	echo "worksheet ".$sheetIndex ." =>" .$sheetName."<br><br>";
 }
 
 
@@ -57,11 +79,20 @@ $courseNumber=11;
 // $courseNumber=2;
 $courseInterval=3; //column used by each course
 $courseFullNameRow=8;
+$courseShortNameRow=$courseFullNameRow-1	;
 $courseFullNameCol=3; //'D'
 $courseLetterOffset=2;
 $coursePointOffset=1;
 
-$course_prefix ="22_23";
+
+
+$nim_col='C';
+$nim_start=10;
+$nim_end=96;
+
+$course_prefix ="22_23"; //moodle course shortname prefix
+
+
 for ($c=1;$c<=$courseNumber;$c++){
 	
 	
@@ -70,41 +101,103 @@ for ($c=1;$c<=$courseNumber;$c++){
 	
 	//2. cek item exist ? if not ... regrade ..
 	
+	$coord=getNameFromNumber($courseFullNameCol).$courseShortNameRow;
+	// echo $coord."<br>";		
+	$shortName=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
+	$shortName=trim($shortName);
+	if($shortName==false) die ("error get course shortname");
+	
 	$coord=getNameFromNumber($courseFullNameCol).$courseFullNameRow;
-	echo $coord."<br>";		
-	$cell=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
-	if($cell==false) die ("error get course name");
-	echo "Course ".$cell."<br>\n";
+	// echo $coord."<br>";		
+	$fullName=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
+	if($fullName==false) die ("error get course name");
+	echo "<BR>\n";
+	echo "<br>in Excel : Course ".$shortName." ".$fullName;
 
-	$nim_col='C';
-	$nim_start=10;
-	$nim_end=96;
+	if (!$course = $DB->get_record('course', array('shortname' => $shortName))) {
+		print_error('invalidcourseshort code'. $shortName);
+	}else {
+		echo "In Moodle: Course id: ".$course->id." ".$course->fullname;
+	}
 	
+	echo "<br>";
+	if ($gitem = $DB->get_record('grade_items', array('courseid' => $course->id))) {
+		echo(' item TOTAL exist ='.$gitem->id);
+	}else {
+		echo(' item TOTAL NOT exist ');
+		die ("Need to create TOTAL grade first");
+		//create_grade_total($DB,$courseid)
+	}
+
+	//create course context first ... 
+	$context = context_course::instance($course->id);
+	if (!$context ){
+		die ("error getting course context");
+	}
+	$grade_letters= grade_get_letters($context) ; //get letters and its 
+	if(!$grade_letters){
+		die("can't get course grade letters ");
+	} 
+	echo "<br>";
+	// var_dump($grade_letters);
+	// foreach ($grade_letters as $num => $let) {
+		// echo $num ."=>". $let." ";
+	// }
+	// echo "<BR>\n";
 	
-	/*
+	// $nim_end=$nim_start; //debugging only
+	
+	/**/
 	for ($nim=$nim_start;  $nim<=$nim_end; $nim++){
+			echo "<br>";
 			$coord=$nim_col.$nim;
-			// echo $nim.": getting ".$coord." => ";
 			 $NIM=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
-			 if($cell==false) die ("error get course name");
+			 if($NIM==false) die ("error get NIM at".$coord);
+
+			 //getting userid
+
+			 
 			 $point_col=$courseFullNameCol+$coursePointOffset;
 			 $coord=getNameFromNumber($point_col).$nim;
 			 $point=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
-			 
+			 // echo "9point coord:  ".$coord." => ".$point . ")";
+			 if($point==false) die ("error get POINT at".$coord);
 			 $letter_col=$courseFullNameCol+$courseLetterOffset;
 			 $coord=getNameFromNumber($letter_col).$nim;
-			 if($cell==false) die ("error get course name");
 			 $letter=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
-			 if($cell){
-				echo $NIM." ".$point." ".$letter."<BR>";
-			 }else {
-					xlsx_err();
-			 }
+			 if($point==false) die ("error get course name");
+			 $letter=$spreadsheet->getActiveSheet()->getCell($coord); // reads D12 cell from second sheet
 			 
-	}
-	
-	*/
+			 echo $NIM." ".$point." ".$letter."<BR>";
+			 $point=floatval($point->getValue());
+			 
+			 
+			 //inserting, updating ..
+			 $cl=trim(getLetterFromPoint($grade_letters,$point));
+			 $letter=trim($letter->getValue());
+			 echo "correct letter for ".$point." is ".$cl;
+			 
+			 if ($cl != $letter){
+				 $point=getPointFromLetter($grade_letters,$letter);
+				 echo " <font color=\"red\"> => LETTER NOT SAME</font><br>";
+				 echo " <font color=\"red\"> => point CORRECTED to ".$point."</font><br>";
+				 
+			 } else {
+				 echo " <font color=\"green\"> => LETTER already SAME</font><br>";
+				 
+			 }
+			 //getting userid
+			 //
+			 // $itemmodule, $iteminstance,$userid_or_ids=null;
+			 // $stgrades=grade_get_grades($courseid, "course", $itemmodule, $iteminstance, $userid_or_ids=null);
 
+			 //grade_update("course", $courseid, $itemtype, $itemmodule, $iteminstance, $itemnumber, $grades=NULL, $itemdetails=NULL) {
+    
+			 
+	} //eo. for ($nim=$nim_start;  $nim<=$nim_end; $nim++){
+	
+	/**/
+	
 	$courseFullNameCol+=$courseInterval;
 }	
 echo "finish...<br>";
